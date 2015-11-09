0e576982db5c8667c6f03507ee18391240def91e
Darius Bacon <darius@wry.me>
2015-11-09 11:56:33 -0500

    just show a grid

---
+++ complex.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var zero = {re: 0, im: 0};
+var one  = {re: 1, im: 0};
+
+function squaredMagnitude(v) {
+    return v.re*v.re + v.im*v.im;
+}
+
+function distance2(u, v) {
+    return squaredMagnitude(sub(u, v));
+}
+
+function add(u, v) {
+    return {re: u.re + v.re,
+            im: u.im + v.im};
+}
+
+function sub(u, v) {
+    return add(u, rmul(-1, v));
+}
+
+function mul(u, v) {
+    return {re: u.re * v.re - u.im * v.im,
+            im: u.im * v.re + u.re * v.im};
+}
+
+function div(u, v) {
+    return mul(u, reciprocal(v));
+}
+
+function reciprocal(v) {
+    var vv = v.re*v.re + v.im*v.im;
+    return rmul(1/vv, conjugate(v));
+}
+
+function conjugate(v) {
+    return {re: v.re,
+            im: -v.im};
+}
+
+function rmul(r, v) {
+    return {re: r * v.re,
+            im: r * v.im};
+}
+
+// An approximate square root of square.
+// Not necessarily the principal one. (How to get that?)
+function roughSqrt(square) {
+    var z = rmul(.5, add(one, square)); // (is this a silly first guess?)
+    for (var i = 10; 0 <= i; --i) {
+        z = rmul(.5, add(z, div(square, z)));
+    }
+    return z;
+}
+
+// An approximate cube root of cube.
+// Not necessarily the principal one. (How to get that?)
+function roughCubeRoot(cube) {
+    var z = roughSqrt(cube);
+    for (var i = 10; 0 <= i; --i) {
+        z = rmul(1/3, add(rmul(2, z),
+                          div(cube, mul(z, z))));
+    }
+    return z;
+}
---
+++ sheet.js
@@ -0,0 +1,213 @@
+// A canvas UI to complex-number arithmetic.
+// Let's call complex numbers 'arrows' here. ComplexNumber would be a mouthful.
+
+'use strict';
+
+var maxClickDistance = 2;
+var minSelectionDistance = 20;
+var dotRadius = 3;
+var selectedDotRadius = 10;
+
+function onLoad() {
+    var quiver = makeQuiver();
+    var sheet = makeSheet(canvas);
+    sheet.drawGrid();
+    sheet.ctx.lineWidth = 1;
+    sheet.ctx.strokeStyle = 'black';
+    sheet.drawLine(one, ({re: 2, im: 1}));
+}
+
+// A quiver is a collection of arrows, with dependencies between some of them.
+// The arrows can move, and can be added or removed from the collection.
+// It's the 'model' with respect to the sheet UI.
+function makeQuiver() {
+
+    var arrows = [];
+
+    function isEmpty() {
+        return 0 < arrows.length;
+    }
+
+    function getArrows() {
+        return arrows;
+    }
+
+    function getFreeArrows() {
+        return arrows.filter(function(arrow) { return arrow.op === variableOp; });
+    }
+
+    function add(arrow) {
+        arrow.label = arrow.op.label(arrow, quiver);
+        arrows.push(arrow);
+        recompute(arrow);
+        return arrow;
+    }
+
+    function onMove() {
+        arrows.forEach(recompute);
+    }
+
+    function recompute(arrow) {
+        arrow.op.recompute(arrow);
+    }
+
+    var quiver = {
+        add: add,
+        isEmpty: isEmpty,
+        getArrows: getArrows,
+        getFreeArrows: getFreeArrows,
+        moveTo: moveTo,
+        onMove: onMove,
+    };
+    return quiver;
+}
+
+var tau = 2*Math.PI;
+
+function makeSheet(canvas, options) {
+    options = override({center:   zero,
+                        font:     '12pt Georgia',
+                        realSpan: 8},
+                       options);
+
+    var ctx    = canvas.getContext('2d');
+    var width  = canvas.width;   // N.B. it's best if these are even
+    var height = canvas.height;
+    var left   = -width/2;
+    var right  =  width/2;
+    var bottom = -height/2;
+    var top    =  height/2;
+    var scale  = width / options.realSpan;
+
+    function pointFromXY(xy) {
+        return {re: xy.x / scale, im: xy.y / scale};
+    }
+
+    ctx.font = options.font;
+    ctx.translate(right, top);
+    ctx.scale(1, -1);
+
+    if (options.center.re !== 0 || options.center.im !== 0) {
+        throw new Error("off-center sheet not supported yet");
+    }
+
+    function clear() {
+        ctx.clearRect(left, bottom, width, height);
+    }
+
+    function drawDot(at, radius) {
+        ctx.beginPath();
+        ctx.arc(scale * at.re, scale * at.im, radius, 0, tau);
+        ctx.fill();
+    }
+
+    function drawLine(at1, at2) {
+        ctx.beginPath();
+        ctx.moveTo(scale * at1.re, scale * at1.im);
+        ctx.lineTo(scale * at2.re, scale * at2.im);
+        ctx.stroke();
+    }
+
+    // N.B. textOffset is in canvas (xy) coordinates.
+    function drawText(at, text, textOffset) {
+        var x = at.re * scale + offset.x;
+        var y = at.im * scale + offset.y;
+        ctx.save();
+        ctx.scale(1, -1); // Back into left-handed coordinates so the text isn't flipped
+        ctx.fillText(text, x, -y);
+        ctx.restore();
+    }
+
+    // Draw an arc from cnum u to uv (which should be u*v).
+    function drawSpiral(u, v, uv) {
+        var zs = computeSpiralArc(u, v, uv);
+        var path = [];
+        zs.forEach(function(z) {
+            path.push(scale * z.re);
+            path.push(scale * z.im);
+        });
+        drawSpline(ctx, path, 0.4, false);
+    }
+
+    function drawGrid() {
+        var i, j;
+        ctx.strokeStyle = 'grey';
+        ctx.lineWidth = 1;
+        for (i = 1; (i-1) * scale <= right; ++i) { // XXX hack
+            ctx.globalAlpha = .25;
+            for (j = 1; j <= 9; ++j) {
+                gridLines((i-1 + j/10) * scale, bottom, (i-1 + j/10) * scale, top);
+            }
+            ctx.globalAlpha = 1;
+            gridLines(i * scale, bottom, i * scale, top);
+        }
+        for (i = 1; (i-1) * scale <= top; ++i) { // XXX hack
+            ctx.globalAlpha = .25;
+            for (j = 1; j <= 9; ++j) {
+                gridLines(left, (i-1 + j/10) * scale, right, (i-1 + j/10) * scale);
+            }
+            ctx.globalAlpha = 1;
+            gridLines(left, i * scale, right, i * scale);
+        }
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(left, -1, width, 3);
+        ctx.fillRect(-1, bottom, 3, height);
+
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.arc(0, 0, scale, 0, tau, true);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    
+    function gridLines(x0, y0, x1, y1) {
+        gridLine(x0, y0, x1, y1);
+        gridLine(-x0, -y0, -x1, -y1);
+    }
+
+    function gridLine(x0, y0, x1, y1) {
+        drawLineXY(x0 - .5, y0 - .5, x1 - .5, y1 - .5); // - .5 for sharp grid lines
+    }
+
+    function drawLineXY(x0, y0, x1, y1) {
+        ctx.beginPath();
+        ctx.moveTo(x0, y0);
+        ctx.lineTo(x1, y1);
+        ctx.stroke();
+    }
+
+    function translate(at) {
+        ctx.translate(at.re * scale, at.im * scale);
+    }
+
+    return {
+        clear: clear,
+        ctx: ctx,
+        drawDot: drawDot,
+        drawGrid: drawGrid,
+        drawLine: drawLine,
+        drawSpiral: drawSpiral,
+        drawText: drawText,
+        pointFromXY: pointFromXY,
+        scale: scale,
+        translate: translate,
+    };
+}
+
+
+// Helpers
+
+function noOp() { }
+
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+var override =
+    (Object.assign ? Object.assign : function(obj1, obj2) {
+        for (var k in obj2) {
+            if (({}).hasOwnProperty.call(obj2, k)) {
+                obj1[k] = obj2[k];
+            }
+        }
+        return obj1;
+    });
