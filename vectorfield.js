344765c29d70e491aa2d0b0284a41a6c345b4d6a
Darius Bacon <darius@wry.me>
2015-11-16 15:27:38 -0500

    streamlines in place of vectors

--- vectorfield.js
+++ vectorfield.js
@@ -2,44 +2,53 @@
 
 'use strict';
 
+var sheet;
 function onLoad() {
-    var sheet = makeSheet(canvas);
+    sheet = makeSheet(canvas);
     sheet.drawGrid();
     sheet.ctx.strokeStyle = 'black';
     drawMap(sheet,
             function(z) {
+//                return mul(z, z);
                 return mul(z, mul(z, z));
+                return mul(z, mul(z, mul(z, z)));
             },
-            0.05, 20);
+            0.05, 15);
 }
 
 function drawMap(sheet, f, vectorScale, spacing) {
     var ctx = sheet.ctx;
-    var scale = sheet.scale;
-    vectorScale *= scale;
-    console.log(vectorScale);
-    ctx.globalAlpha = 0.5;
+    ctx.globalAlpha = 0.25;
     var height = sheet.canvas.height;
     var width  = sheet.canvas.width;
     for (var y = 0; y < height; y += spacing) {
         for (var x = 0; x < width; x += spacing) {
-            var z0 = sheet.pointFromXY({x: x, y: y});
-            var z1 = f(z0);
-            var x0 = scale * z0.re;  // XXX ugh
-            var y0 = scale * z0.im;
-            var dx = vectorScale * z1.re;
-            var dy = vectorScale * z1.im;
-            var nsteps = 5;
-            for (var i = 0; i < nsteps; ++i) {
-                ctx.lineWidth = (nsteps-i) * 0.5;
-                ctx.beginPath();
-                ctx.moveTo(x0, y0);
-                x0 += dx/nsteps;
-                y0 += dy/nsteps;
-                ctx.lineTo(x0, y0);
-//            console.log(vectorScale * z1.re, vectorScale * z1.im);
-                ctx.stroke();
-            }
+            var z = sheet.pointFromXY({x: x, y: y});
+            drawStreamline(sheet, z, f, vectorScale);
         }
+    }
+}
+
+function drawStreamline(sheet, z, f, vectorScale) {
+    var ctx = sheet.ctx;
+    var scale = sheet.scale;
+    var nsteps = 10;
+    for (var i = 0; i < nsteps; ++i) {
+        ctx.lineWidth = (nsteps-i) * 0.5;
+        var dz = rmul(vectorScale/nsteps, f(z));
+        if (1 && (scale*scale)*(0.001) < squaredMagnitude(dz)) {
+            // We going too far and might end up with random-looking
+            // sharp-angled paths. Stop and let this streamline get
+            // approximately filled in from some other starting point.
+            break;
+        }
+        var z1 = add(z, dz);
+
+        ctx.beginPath();
+        ctx.moveTo(scale*z.re, scale*z.im);
+        ctx.lineTo(scale*z1.re, scale*z1.im);
+        ctx.stroke();
+
+        z = z1;
     }
 }
