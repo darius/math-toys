6e285841e764cd4fd16280e75d15315e47380dde
Mary Rose Cook <mary@maryrosecook.com>
2015-11-17 17:22:18 -0500

    Update testsheet to use newly moduled variables

--- testsheet.js
+++ testsheet.js
@@ -16,17 +16,17 @@ function tempTest() {
     sheetObj.ctx.lineWidth = 1;
     sheetObj.ctx.strokeStyle = 'black';
     sheetObj.ctx.fillStyle = 'blue';
-    sheetObj.drawDot(one, 3);
-    sheetObj.drawLine(one, {re: 2, im: 1});
-    sheetObj.drawText(one, '1', {x: -14, y: 10});
-    sheetObj.drawSpiral(one, {re: 2, im: 1}, {re: 2, im: 1});
+    sheetObj.drawDot(complex.one, 3);
+    sheetObj.drawLine(complex.one, {re: 2, im: 1});
+    sheetObj.drawText(complex.one, '1', {x: -14, y: 10});
+    sheetObj.drawSpiral(complex.one, {re: 2, im: 1}, {re: 2, im: 1});
     console.log(sheetObj.pointFromXY({x: 400, y: 0}));
 
-    var i = quiver.add({op: constantOp, at: {re: 0, im: 1}});
-    sheetObj.drawDot(i.at, dotRadius);
+    var i = quiver.add({op: sheet.constantOp, at: {re: 0, im: 1}});
+    sheetObj.drawDot(i.at, sheet.dotRadius);
     sheetObj.drawText(i.at, i.label, i.op.labelOffset);
 
-    var v = quiver.add({op: variableOp, at: {re: 2.1, im: -1}});
-    sheetObj.drawDot(v.at, dotRadius);
+    var v = quiver.add({op: sheet.variableOp, at: {re: 2.1, im: -1}});
+    sheetObj.drawDot(v.at, sheet.dotRadius);
     sheetObj.drawText(v.at, v.label, v.op.labelOffset);
 }
